95ade883b4103918af5af264c7ae206652218bb0
ilyesBB <ilyes.boussama@yahoo.fr>
2018-04-09 23:08:21 +0200

    oubli

---
+++ gl_lib/sim/robot/display/d2/gui/AppSimulation.py
@@ -0,0 +1,70 @@
+from tkinter import *
+from gl_lib.sim.simulation import Simulation
+from gl_lib.sim.geometry import *
+from gl_lib.sim.robot.display.d2.view import Vue2DArene
+from threading import Thread
+from gl_lib.config import PAS_TEMPS
+
+
+class AppSimulation(Tk):
+    """
+    Definit une structure d'display d'un robot dans une arene vide
+    """
+
+    def __init__(self, simulation: Simulation):
+        """
+
+        :param simulation:
+        """
+        Tk.__init__(self)
+        self.simulation = simulation
+        if self.simulation.arene is not None:
+            self.vue_arene = Vue2DArene(self.simulation.arene)
+        else:
+            a = Arene()
+            a.add(self.simulation.strategie.robot)
+            self.vue_arene = Vue2DArene(a)
+
+        self.stop = True
+
+        self.canvas = Canvas(self, height=450, width=600, bg='white')
+
+        self.canvas.bind("<Button-1>", self.start)
+        self.canvas.bind("<Button-2>", self.stop)
+        self.vue_arene.afficher(self.canvas)
+
+        self.canvas.pack()
+
+    def start(self, event):
+        """
+        lance la simulation
+        :return:
+        """
+        self.stop = False
+        self.update()
+
+    def update(self):
+        """
+        Met a jour les vitesses
+        """
+        self.canvas.delete(ALL)
+        self.vue_arene.afficher(self.canvas)
+
+        self.after(int(100), self.update)
+
+
+class AppSimulationThread(Thread):
+    def __init__(self, simulation):
+        Thread.__init__(self)
+        self.simulation = simulation
+
+    def run(self):
+        self.window = AppSimulation(self.simulation)
+        self.window.mainloop()
+
+
+if __name__ == '__main__':
+    from gl_lib.sim.robot import RobotMotorise
+    from gl_lib.sim.geometry.point import Point
+
+    r = RobotMotorise(Pave(30, 30, 0, centre=Point(100, 100, 0)))
---
+++ gl_lib/sim/robot/display/d2/view/Vue2DVecteur.py
@@ -0,0 +1,21 @@
+from gl_lib.sim.robot.display.d2.view.Vue2D import Vue2D
+from gl_lib.sim.geometry import *
+from tkinter import *
+from gl_lib.config import PIX_PAR_M
+class Vue2DVecteur(Vue2D):
+    """ Classe contenant un vecteur et sa representation2D """
+    def __init__(self, vecteur, canevas):
+        """Constructeur de la view
+        vecteur: Vecteur
+        ligne: ligne 2D
+        canevas: Canevas
+        
+        Cree la ligne dans canevas
+        """
+        self.vecteur=vecteur
+        self.ligne=canevas.create_line(0, 0, vecteur.x*PIX_PAR_M, vecteur.y*PIX_PAR_M)
+        
+    def afficher(self, canevas, position):
+        """ actualise la position de ligne """
+
+        canevas.coords(self.ligne, position.x*PIX_PAR_M, position.y*PIX_PAR_M, (position.x+self.vecteur.x)*PIX_PAR_M, (position.y+self.vecteur.y)*PIX_PAR_M)
